e68e268cbb148a78aefad2f8c20e379e5f8053b8
Titouan CREACH <titouan.creach@eegle.io>
2017-08-18 18:16:53 +0200

    compile with babel after typescript

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
       }
     ]
   }
